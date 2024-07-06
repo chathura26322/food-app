7a63c3ab5c2f2bc60ba587ab1f84b876a7810640
chathura26322 <chathura26322@gmail.com>
2024-07-06 15:48:32 +0530

    completed the menu section

--- src/components/ExploreMenu.jsx
+++ src/components/ExploreMenu.jsx
@@ -2,48 +2,39 @@ import { useState } from "react";
 
 const ExploreMenu = () => {
   const [menu, setMenu] = useState("Rice");
+
+  const handleClick = (menuItem) => {
+    setMenu(menuItem);
+  };
+
   return (
     <div>
       <div className="flex justify-between py-[30px] px-[40px]  h-[150px]">
         <div className="flex overflow-x-scroll gap-[100px]  relative ">
-          <p
-            className={`border-[3px] border-[#A70604] rounded-[25px] font-medium text-black bg-white min-w-[110px] p-[3px] h-[35px] text-center align-middle  cursor-pointer ${
-              menu === "Rice" ? "bg-[#A70604] text-white" : ""
-            }`}
-            onClick={() => setMenu("Rice")}
-          >
-            Rice
-          </p>
-          <p className="border-[3px] border-[#A70604] rounded-[25px] font-medium text-black bg-white min-w-[110px] p-[3px] h-[35px] text-center align-middle cursor-pointer">
-            Noodles
-          </p>
-          <p className="  border-[3px] border-[#A70604] rounded-[25px] font-medium text-black bg-white min-w-[110px] p-[3px] h-[35px] text-center align-middle cursor-pointer">
-            FastFood
-          </p>
-          <p className="  border-[3px] border-[#A70604] rounded-[25px] font-medium text-black bg-white min-w-[110px] p-[3px] h-[35px] text-center align-middle cursor-pointer ">
-            Pure Veg
-          </p>
-          <p className=" border-[3px] border-[#A70604] rounded-[25px] font-medium text-black bg-white min-w-[110px] p-[3px] h-[35px] text-center align-middle cursor-pointer ">
-            Beverages
-          </p>
-          <p className="  border-[3px] border-[#A70604] rounded-[25px] font-medium text-black bg-white min-w-[110px] p-[3px] h-[35px] text-center align-middle  cursor-pointer">
-            Ice Cream
-          </p>
-          <p className="  border-[3px] border-[#A70604] rounded-[25px] font-medium text-black bg-white min-w-[110px] p-[3px] h-[35px] text-center align-middle cursor-pointer ">
-            Desserts
-          </p>
-          <p className=" border-[3px] border-[#A70604] rounded-[25px] font-medium text-black bg-white min-w-[110px] p-[3px] h-[35px] text-center align-middle cursor-pointer ">
-            Salads
-          </p>
-          <p className=" border-[3px] border-[#A70604] rounded-[25px] font-medium text-black bg-white min-w-[110px] p-[3px] h-[35px] text-center align-middle  cursor-pointer">
-            Pizza
-          </p>
-          <p className=" border-[3px] border-[#A70604] rounded-[25px] font-medium text-black bg-white min-w-[110px] p-[3px] h-[35px] text-center align-middle cursor-pointer ">
-            Snacks
-          </p>
+          {[
+            "Rice",
+            "Noodles",
+            "FastFood",
+            "Pure Veg",
+            "Beverages",
+            "Ice Cream",
+            "Desserts",
+            "Salads",
+            "Pizza",
+            "Snacks",
+          ].map((menuItem) => (
+            <p
+              key={menuItem} // Add a unique key for each menu item
+              className={`border-[3px] border-[#A70604] rounded-[25px] font-medium text-black  min-w-[110px] p-[3px] h-[35px] text-center align-middle cursor-pointer ${
+                menu === menuItem ? "bg-[#A70604] text-white" : ""
+              }`}
+              onClick={() => handleClick(menuItem)}
+            >
+              {menuItem}
+            </p>
+          ))}
         </div>
       </div>
-      <hr className="w-[100%] " />
     </div>
   );
 };
